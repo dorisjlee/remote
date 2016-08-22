cceee1b79505709fe2c0f5cceb725b2f72be5db3
dorislee0309 <dorislee@berkeley.edu>
2016-08-22 13:33:51 -0500

    update

--- startup.py
+++
@@ -1,5 +0,0 @@
-#This file should be located inside .ipython/profile_default/startup/startup.py
-#Note that //anaconda loads up NERSC defaults
-import sys
-sys.path.remove('//anaconda/lib/python2.7/site-packages')
-sys.path.append('/global/homes/d/dorislee/anaconda/lib/python2.7/site-packages')  
